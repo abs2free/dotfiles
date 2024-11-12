6028c1ba09a6d49dd2237f491f65f15c646e27cb
abs2free <abs2free@gmail.com>
2024-11-12 09:12:05 +0800

    up

--- link.sh
+++ link.sh
@@ -1,8 +1,8 @@
 cd ~
-ln -s ~/abs2free/github.com/dotfiles/.zshrc
-ln -s ~/abs2free/github.com/dotfiles/.bashrc
-ln -s ~/abs2free/github.com/dotfiles/.fzf.bash
-ln -s ~/abs2free/github.com/dotfiles/.fzf.zsh
-ln -s ~/abs2free/github.com/dotfiles/.tmux-layouts
-ln -s ~/abs2free/github.com/dotfiles/.tmux.conf
-ln -s ~/abs2free/github.com/dotfiles/.zprofile
+ln -s ~/github.com/dotfiles/.zshrc
+ln -s ~/github.com/dotfiles/.bashrc
+ln -s ~/github.com/dotfiles/.fzf.bash
+ln -s ~/github.com/dotfiles/.fzf.zsh
+ln -s ~/github.com/dotfiles/.tmux-layouts
+ln -s ~/github.com/dotfiles/.tmux.conf
+ln -s ~/github.com/dotfiles/.zprofile
